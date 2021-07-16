bddcf2c7c50056a9712a45868bc15a80aca617e2
Daniel Barboza da Silva <daniel.bsilva@al.infnet.edu.br>
2021-07-16 11:39:16 -0300

    Config Tabela

--- src/store/cliente/cliente.action.js
+++ src/store/cliente/cliente.action.js
@@ -25,7 +25,7 @@ export const getAll = () => {
     try {
       dispatch({ type: TYPES.CLIENTE_LOADING, status: true })
       const result = await getAllCliente()
-      
+
       dispatch({ type: TYPES.CLIENTE_ALL, data: result.data })
     } catch (error) {
       toastr.error('aconteceu um erro', error)
