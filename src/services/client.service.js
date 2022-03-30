0ab467452a8aedea30d8eb4195653f759bd4fa51
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-03-30 00:47:45 -0300

    feature: ensure list client by id

--- src/services/client.service.js
+++ src/services/client.service.js
@@ -5,4 +5,6 @@ export const listAllClientService = () => http.get(baseUrl)
 
 export const listByIdClientService = (id) => http.get(`${baseUrl}/${id}`)
 
+export const listLikeByIdClientService = (id) => http.get(`${baseUrl}/${id}/like`)
+
 export const createClientService = (data) => http.post(baseUrl, data)
