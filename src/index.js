1ad72df8aeb626cced3fb07d21c5fe25c5887cca
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-07-27 18:19:29 -0300

    chore: add google font great vibes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux'
 import reportWebVitals from './reportWebVitals'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Routers from './routers'
+import { Helmet } from 'react-helmet'
 import ReduxToastr from './components/redux-toastr'
 import './assets/css/style.css'
 import GlobalStyle from './config/globalStyled'
@@ -14,10 +15,16 @@ import 'react-redux-toastr/lib/css/react-redux-toastr.min.css'
 import { ThemeProvider } from 'styled-components'
 import theme from './plugins/theme'
 
+const googleFontGreatVibes =
+  'https://fonts.googleapis.com/css2?family=Cormorant:wght@300&family=Great+Vibes&display=swap'
+
 ReactDOM.render(
   <Provider store={store}>
     <ReduxToastr />
     <ThemeProvider theme={theme}>
+      <Helmet>
+        <link rel="stylesheet" href={googleFontGreatVibes} />
+      </Helmet>
       <GlobalStyle />
       <CssBaseline />
       <Routers />
