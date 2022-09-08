200fbb812b1fffc71a14a4bbd0e4cd3891d48020
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-09-08 00:49:43 -0300

    refactor: add visual improvements

--- src/components/admin/categories/remove.js
+++ src/components/admin/categories/remove.js
@@ -1,41 +1,37 @@
-import {
-  Button,
-  Card,
-  CardActionArea,
-  CardActions,
-  CardContent,
-  Typography
-} from '@material-ui/core'
+import * as React from 'react'
+import Button from '@mui/material/Button'
+import Dialog from '@mui/material/Dialog'
+import DialogActions from '@mui/material/DialogActions'
+import DialogContent from '@mui/material/DialogContent'
+import DialogContentText from '@mui/material/DialogContentText'
+import DialogTitle from '@mui/material/DialogTitle'
 
-const Remove = ({ remove, close }) => {
+const Remove = ({ open, remove, close }) => {
   return (
-    <Card>
-      <CardActionArea>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Deseja Excluir a Categoria ?
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions style={{ display: 'flex', justifyContent: 'space-around' }}>
-        <Button
-          size="small"
-          onClick={remove}
-          variant="contained"
-          color="primary"
-        >
-          Sim
-        </Button>
-        <Button
-          size="small"
-          onClick={close}
-          variant="contained"
-          color="secondary"
-        >
-          Não
-        </Button>
-      </CardActions>
-    </Card>
+    <div>
+      <Dialog
+        open={open}
+        onClose={close}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+        disableEnforceFocus
+      >
+        <DialogTitle id="alert-dialog-title">Excluir Categoria</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Tem certeza de que deseja excluir a categoria selecionada?
+            <br /> Esta ação não poderá ser desfeita!
+          </DialogContentText>
+        </DialogContent>
+
+        <DialogActions>
+          <Button onClick={close}>CANCELAR</Button>
+          <Button onClick={remove} autoFocus>
+            <strong>CONFIRMAR</strong>
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
   )
 }
 
